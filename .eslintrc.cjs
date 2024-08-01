ec4ff2848ac963b6593b310380abb880fd4508fe
sedykhvitaliy <sedykhvit@gmail.com>
2024-08-01 18:30:59 +0300

    init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,13 +18,4 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'react/prop-types': 'off', // add this line
-    'react/no-unescaped-entities': 'off' // add this line
-  },
-  
 }
